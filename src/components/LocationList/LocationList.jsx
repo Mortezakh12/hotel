a016b878d0f3060f7accaf9efe1386aa6c5110b0
Mortezakh12 <114151002+Mortezakh12@users.noreply.github.com>
2023-09-05 22:39:49 +0330

    Added Toaster and LocationList components

---
+++ src/components/LocationList/LocationList.jsx
@@ -0,0 +1,29 @@
+import useFetch from "../../Hooks/useFetch";
+
+const LocationList = () => {
+  const { data, isLoading } = useFetch("http://localhost:5000/hotels", "");
+  if (isLoading) <p>Loading...</p>;
+  return (
+    <div className="nearbyLocation">
+      <h2>Nearby Hotels</h2>
+      <div className="locationList">
+        {data.map((item) => {
+          return (
+            <div key={item.id} className="locationItem">
+              <img src={item.picture_url.url} alt={item.name} />
+              <div className="locationItemDesc">
+                <p className="location">{item.smart_location}</p>
+                <p className="name">{item.name}</p>
+                <p className="price">
+                  Є&nbsp;{item.price}&nbsp;
+                  <span>night</span>
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default LocationList;
